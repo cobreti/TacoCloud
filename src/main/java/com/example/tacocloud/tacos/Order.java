9bb4ab7ae6ce02b0887515fba3287551b8a04ce7
dannyt <cobreti_@hotmail.com>
2021-04-20 17:41:36 -0400

    added some validations

--- src/main/java/com/example/tacocloud/tacos/Order.java
+++ src/main/java/com/example/tacocloud/tacos/Order.java
@@ -1,94 +1,35 @@
 package com.example.tacocloud.tacos;
 
-import java.util.Objects;
+import lombok.Data;
 
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Data
 public class Order {
+    @NotBlank(message="Name is required")
     private String name;
+
+    @NotBlank(message="Street is required")
     private String street;
+
+    @NotBlank(message="City is required")
     private String city;
+
+    @NotBlank(message="State is required")
     private String state;
+
+    @NotBlank(message="Zip code is required")
     private String zip;
+
+
     private String ccNumber;
+
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
+            message="Must be formatted MM/YY")
     private String ccExpiration;
+
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
-
-    public Order() {
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Order)) return false;
-        Order order = (Order) o;
-        return Objects.equals(name, order.name) && Objects.equals(street, order.street) && Objects.equals(city, order.city) && Objects.equals(state, order.state) && Objects.equals(zip, order.zip) && Objects.equals(ccNumber, order.ccNumber) && Objects.equals(ccExpiration, order.ccExpiration) && Objects.equals(ccCVV, order.ccCVV);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, street, city, state, zip, ccNumber, ccExpiration, ccCVV);
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getCcNumber() {
-        return ccNumber;
-    }
-
-    public void setCcNumber(String ccNumber) {
-        this.ccNumber = ccNumber;
-    }
-
-    public String getCcExpiration() {
-        return ccExpiration;
-    }
-
-    public void setCcExpiration(String ccExpiration) {
-        this.ccExpiration = ccExpiration;
-    }
-
-    public String getCcCVV() {
-        return ccCVV;
-    }
-
-    public void setCcCVV(String ccCVV) {
-        this.ccCVV = ccCVV;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
